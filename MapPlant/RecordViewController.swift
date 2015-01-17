3343aefaa44b35d3631403019a13d65a2add6e69
Kevin Sullivan <admin@sideapps.com>
2015-01-16 19:46:35 -0500

    Location Updates + Polyline
    Added functionality to draw a path following the GPS location
    
    Signed-off-by: Kevin Sullivan <admin@sideapps.com>

--- MapPlant/RecordViewController.swift
+++ MapPlant/RecordViewController.swift
@@ -10,9 +10,9 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class RecordViewController: UIViewController, CLLocationManagerDelegate {
+class RecordViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     // Map variables
-//    @IBOutlet weak var mapView: MKMapView!
+    //    @IBOutlet weak var mapView: MKMapView!
     var mapView = MKMapView()
     
     // Location variables
@@ -22,8 +22,25 @@ class RecordViewController: UIViewController, CLLocationManagerDelegate {
     // Logger
     let logger = Swell.getLogger("RecordViewController")
     
+    // Device details
+    var width: CGFloat = 0
+    var height: CGFloat = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Grab device details
+        width = self.view.frame.size.width
+        height = self.view.frame.size.height
+        
+        // Create and configure the map
+        mapView = MKMapView(frame: CGRect(x: 0, y: 0, width: width, height: height / 2))
+        mapView.showsUserLocation = true
+        mapView.mapType = MKMapType.Standard
+        mapView.delegate = self
+        
+        // Add our subviews to main view
+        self.view.addSubview(mapView)
         
         // Configure map view
         mapView.showsUserLocation = true
@@ -45,14 +62,16 @@ class RecordViewController: UIViewController, CLLocationManagerDelegate {
     
     // MARK: - Location manager delegates
     
+    // Handle location manager failures
     func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {
         locationManager.stopUpdatingLocation()
-
+        
         if (error != nil) {
             Swell.error("Location manager failed with error: \(error)")
         }
     }
     
+    // Log location updates
     func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
         let location = insertObject(Const.Data.Location) as Location
         
@@ -67,5 +86,51 @@ class RecordViewController: UIViewController, CLLocationManagerDelegate {
         }
         
         lastLoc = location
+        save()
+    }
+    
+    // MARK: - Map delegates
+    
+    // Handle user location updates passed to the map
+    func mapView(mapView: MKMapView!, didUpdateUserLocation userLocation: MKUserLocation!) {
+        // Gather location info
+        var location = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
+        var span = MKCoordinateSpan(latitudeDelta: 0.005, longitudeDelta: 0.005)
+        var region = MKCoordinateRegion(center: location, span: span)
+        
+        // Zoom to the generated region
+        mapView.setRegion(region, animated: true)
+        
+        Swell.debug("Map received user location update")
+        
+        // Retrieve all locations
+        let locations = getObjects(Const.Data.Location, nil) as [Location]
+        
+        // If we have at least two locations, create a polyline from all locations
+        if locations.count > 1 {
+            var coords = [CLLocationCoordinate2D]()
+            
+            for location in locations {
+                let curCoord = CLLocationCoordinate2D(latitude: location.lat as CLLocationDegrees, longitude: location.long as CLLocationDegrees)
+                coords.append(curCoord)
+            }
+            
+            let polyline = MKPolyline(coordinates: &coords, count: coords.count)
+            
+            mapView.addOverlay(polyline)
+        }
+    }
+
+    // Handle polyline drawing
+    func mapView(mapView: MKMapView!, rendererForOverlay overlay: MKOverlay!) -> MKOverlayRenderer! {
+        if overlay is MKPolyline {
+            let renderer = MKPolylineRenderer(overlay: overlay)
+            renderer.strokeColor = UIColor.redColor()
+            renderer.lineWidth = 4
+        
+            return renderer
+        }
+        
+        return nil
     }
 }
